fbee228c6a1756d741e65bf17d058776c4ecbf04
Breno Pires <brenopsantos1@gmail.com>
2024-10-17 19:33:31 -0300

    feat: metodo de listar categorias

--- Code/src/main/java/service/ArquivoCategoria.java
+++ Code/src/main/java/service/ArquivoCategoria.java
@@ -5,45 +5,37 @@ import java.util.ArrayList;
 import model.Categoria;
 
 /**
- *  ArquivoCategoria: Classe que representa um arquivo de categorias.
+ * ArquivoCategoria: Classe que representa um arquivo de categorias.
  */
-public class ArquivoCategoria extends Arquivo<Categoria>
-{
+public class ArquivoCategoria extends Arquivo<Categoria> {
     Arquivo<Categoria> arqTarefa;
     ArvoreBMais<ParNomeIDCategoria> indiceIndiretoNome;
 
-    public ArquivoCategoria ( ) throws Exception 
-    {
-        super( "Categorias.db", Categoria.class.getConstructor() );
-        indiceIndiretoNome = new ArvoreBMais<>
-        ( 
-            ParNomeIDCategoria.class.getConstructor(),
-            5, 
-            ".\\Code\\src\\main\\data\\indiceIndiretoNome.bptree.db" 
-        );
+    public ArquivoCategoria() throws Exception {
+        super("Categorias.db", Categoria.class.getConstructor());
+        indiceIndiretoNome = new ArvoreBMais<>(
+                ParNomeIDCategoria.class.getConstructor(),
+                5,
+                ".\\Code\\src\\main\\data\\indiceIndiretoNome.bptree.db");
     } // end ArquivoCategoria ( )
 
     @Override
-    public int create ( Categoria obj ) throws Exception 
-    {
+    public int create(Categoria obj) throws Exception {
         int id = super.create(obj);
-        indiceIndiretoNome.create( new ParNomeIDCategoria(obj.getNome(), id) );
+        indiceIndiretoNome.create(new ParNomeIDCategoria(obj.getNome(), id));
         return id;
     } // end create ( )
 
-    public Categoria read ( String nome ) throws Exception 
-    {
-        ArrayList<ParNomeIDCategoria> picn = indiceIndiretoNome.read( new ParNomeIDCategoria(nome, -1) );
+    public Categoria read(String nome) throws Exception {
+        ArrayList<ParNomeIDCategoria> picn = indiceIndiretoNome.read(new ParNomeIDCategoria(nome, -1));
         return super.read(picn.get(0).getIDCategoria());
     } // end read ( )
-    
-    public boolean delete ( int nome ) throws Exception 
-    {
+
+    public boolean delete(int nome) throws Exception {
         boolean result = false;
         Categoria obj = super.read(nome);
-        if( obj != null ) 
-        {
-            if( indiceIndiretoNome.delete( new ParNomeIDCategoria(obj.getNome(), obj.getId()) ) ) {
+        if (obj != null) {
+            if (indiceIndiretoNome.delete(new ParNomeIDCategoria(obj.getNome(), obj.getId()))) {
                 result = super.delete(obj.getId());
             } // end if
         } // end if
@@ -51,22 +43,31 @@ public class ArquivoCategoria extends Arquivo<Categoria>
     } // end delete ( )
 
     @Override
-    public boolean update ( Categoria novaCategoria ) throws Exception 
-    {
+    public boolean update(Categoria novaCategoria) throws Exception {
         boolean result = false;
-        Categoria categoriaAntiga = super.read( novaCategoria.getId() );
-        if( super.update(novaCategoria) ) 
-        {
-            if( novaCategoria.getNome() != categoriaAntiga.getNome() ) 
-            {
-                if( indiceIndiretoNome.delete( new ParNomeIDCategoria(categoriaAntiga.getNome(), categoriaAntiga.getId()) ) ) 
-                {
-                    indiceIndiretoNome.create( new ParNomeIDCategoria(novaCategoria.getNome(), novaCategoria.getId()) );
+        Categoria categoriaAntiga = super.read(novaCategoria.getId());
+        if (super.update(novaCategoria)) {
+            if (novaCategoria.getNome() != categoriaAntiga.getNome()) {
+                if (indiceIndiretoNome
+                        .delete(new ParNomeIDCategoria(categoriaAntiga.getNome(), categoriaAntiga.getId()))) {
+                    indiceIndiretoNome.create(new ParNomeIDCategoria(novaCategoria.getNome(), novaCategoria.getId()));
                 } // end if
                 result = true;
             } // end if
         } // end if
         return result;
     } // end update ( )
+
+    // metodo para listar todas as categorias
+    public ArrayList<Categoria> readAll() throws Exception {
+        ArrayList<Categoria> categorias = new ArrayList<>();
+        for (int i = 0; i <= super.readNextId(); i++) {
+            Categoria categoria = super.read(i);
+            if (categoria != null) {
+                categorias.add(categoria);
+            } // end if
+        } // end for
+        return categorias;
+    } // end readAll ( )
 
 } // end class ArquivoCategoria
