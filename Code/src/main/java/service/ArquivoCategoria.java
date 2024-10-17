a0c999de6928f26bd0348c59fc61e245a8c346dc
Vinícius Miranda de Araújo <vinicius.araujo.1457564@sga.pucminas.br>
2024-10-16 21:51:57 -0300

    feat: inversão da ordem dos valores de ParIdId e consequentemente mudança na classe ArquivoCategoria

--- Code/src/main/java/service/ArquivoCategoria.java
+++ Code/src/main/java/service/ArquivoCategoria.java
@@ -10,14 +10,14 @@ import model.Categoria;
 public class ArquivoCategoria extends Arquivo<Categoria>
 {
     Arquivo<Categoria> arqTarefa;
-    ArvoreBMais<ParIDCategoriaNome> indiceIndiretoNome;
+    ArvoreBMais<ParNomeIDCategoria> indiceIndiretoNome;
 
     public ArquivoCategoria ( ) throws Exception 
     {
         super( "Categorias.db", Categoria.class.getConstructor() );
         indiceIndiretoNome = new ArvoreBMais<>
         ( 
-            ParIDCategoriaNome.class.getConstructor(),
+            ParNomeIDCategoria.class.getConstructor(),
             5, 
             ".\\Code\\src\\main\\data\\indiceIndiretoNome.bptree.db" 
         );
@@ -27,15 +27,14 @@ public class ArquivoCategoria extends Arquivo<Categoria>
     public int create ( Categoria obj ) throws Exception 
     {
         int id = super.create(obj);
-        indiceIndiretoNome.create( new ParIDCategoriaNome(id, obj.getNome()) );
+        indiceIndiretoNome.create( new ParNomeIDCategoria(obj.getNome(), id) );
         return id;
     } // end create ( )
 
     public Categoria read ( String nome ) throws Exception 
     {
-        ArrayList<ParIDCategoriaNome> picn = indiceIndiretoNome.read( new ParIDCategoriaNome(-1, nome) );
+        ArrayList<ParNomeIDCategoria> picn = indiceIndiretoNome.read( new ParNomeIDCategoria(nome, -1) );
         return super.read(picn.get(0).getIDCategoria());
-        
     } // end read ( )
     
     public boolean delete ( int nome ) throws Exception 
@@ -44,7 +43,7 @@ public class ArquivoCategoria extends Arquivo<Categoria>
         Categoria obj = super.read(nome);
         if( obj != null ) 
         {
-            if( indiceIndiretoNome.delete( new ParIDCategoriaNome(obj.getId(), obj.getNome()) ) ) {
+            if( indiceIndiretoNome.delete( new ParNomeIDCategoria(obj.getNome(), obj.getId()) ) ) {
                 result = super.delete(obj.getId());
             } // end if
         } // end if
@@ -60,9 +59,9 @@ public class ArquivoCategoria extends Arquivo<Categoria>
         {
             if( novaCategoria.getNome() != categoriaAntiga.getNome() ) 
             {
-                if( indiceIndiretoNome.delete( new ParIDCategoriaNome(novaCategoria.getId(), categoriaAntiga.getNome()) ) ) 
+                if( indiceIndiretoNome.delete( new ParNomeIDCategoria(categoriaAntiga.getNome(), categoriaAntiga.getId()) ) ) 
                 {
-                    indiceIndiretoNome.create( new ParIDCategoriaNome(novaCategoria.getId(), novaCategoria.getNome()) );
+                    indiceIndiretoNome.create( new ParNomeIDCategoria(novaCategoria.getNome(), novaCategoria.getId()) );
                 } // end if
                 result = true;
             } // end if
