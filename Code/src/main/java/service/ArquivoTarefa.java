1ea2ded18cc8732abf0d857f5bbcfe24c51a6d15
Vinícius Miranda de Araújo <vinicius.araujo.1457564@sga.pucminas.br>
2024-10-20 16:26:48 -0300

    feat: mudança de nome de variável e indentação

--- Code/src/main/java/service/ArquivoTarefa.java
+++ Code/src/main/java/service/ArquivoTarefa.java
@@ -1,102 +1,148 @@
 package service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import app.IO;
-import model.Categoria;
 import model.Tarefa;
-
-import java.io.IOException;
 
 /**
  * ArquivoTarefa: Classe que representa um arquivo de tarefas.
  */
-public class ArquivoTarefa extends Arquivo<Tarefa> {
-    ArvoreBMais<ParIDCategoriaIDTarefa> indiceIndiretoIDCategoria;
+public class ArquivoTarefa extends Arquivo<Tarefa> 
+{
+    ArvoreBMais<ParIDCategoriaIDTarefa> arvore;
 
-    public ArquivoTarefa() throws Exception {
-        super("Tarefas.db", Tarefa.class.getConstructor());
-        indiceIndiretoIDCategoria = new ArvoreBMais<>(
-                ParIDCategoriaIDTarefa.class.getConstructor(),
-                5,
-                ".\\Code\\src\\main\\data\\indiceIndiretoIDCategoria.bptree.db");
+    public ArquivoTarefa ( ) throws Exception 
+    {
+        super( "Tarefas.db", Tarefa.class.getConstructor() );
+        arvore = new ArvoreBMais<>
+        ( 
+            ParIDCategoriaIDTarefa.class.getConstructor(),
+            5, 
+            ".\\Code\\src\\main\\data\\Tarefas.db.bpt.idx" 
+        );
     } // end ArquivoTarefa ( )
 
     @Override
-    public int create(Tarefa obj) throws Exception {
-        int id = super.create(obj);
-        indiceIndiretoIDCategoria.create(new ParIDCategoriaIDTarefa(obj.getIdCategoria(), id));
+    public int create ( Tarefa obj ) throws Exception 
+    {
+        int id = super.create( obj );
+        try {
+            arvore.create( new ParIDCategoriaIDTarefa(obj.getIdCategoria(), obj.getId()) );
+        } catch( Exception e ) {
+            System.out.print( "" );
+        } // end try-catch
         return id;
     } // end create ( )
 
     @Override
-    public Tarefa read(int idCategoria) throws Exception {
-        ArrayList<ParIDCategoriaIDTarefa> picit = indiceIndiretoIDCategoria
-                .read(new ParIDCategoriaIDTarefa(idCategoria, -1));
-        return super.read(picit.get(0).getIDTarefa());
+    public Tarefa read ( int idCategoria ) throws Exception 
+    {
+        ArrayList<ParIDCategoriaIDTarefa> picit = arvore.read( new ParIDCategoriaIDTarefa(idCategoria, -1) );
+        return super.read( picit.get(0).getIDTarefa() );
     } // end read ( )
 
-    // readall
-    public List<Tarefa> readAll() throws Exception {
-        List<Tarefa> tarefas = new ArrayList<>();
+    /*     
+    public ArrayList<Tarefa> readAll ( int idCategoria ) throws Exception 
+    {
+        ArrayList<ParIDCategoriaIDTarefa> picit = arvore.read( new ParIDCategoriaIDTarefa(idCategoria, -1) );
+        ArrayList<Tarefa> tarefas = new ArrayList<>( );
+        int i = 0;
+        for( ParIDCategoriaIDTarefa p : picit ) 
+        {
+            System.out.println( picit.get(i));
+            tarefas.add( super.read( p.getIDTarefa() ) );
+            i++;
+        } // end for
+        return tarefas;
+    } // end readAll ( ) 
+     */
+
+    public List<Tarefa> readAll ( ) throws Exception 
+    {
+        List<Tarefa> tarefas = new ArrayList<>( );
 
         arquivo.seek(TAM_CABECALHO);
         byte lapide = ' ';
         short tam = 0;
         byte[] b = null;
 
-        Tarefa c = null;
-
-        // Lê até o final do arquivo
-        while (arquivo.getFilePointer() < arquivo.length()) {
+        Tarefa t = null;
+        while( arquivo.getFilePointer() < arquivo.length() ) 
+        {
             lapide = arquivo.readByte();
             tam = arquivo.readShort();
             b = new byte[tam];
             arquivo.read(b);
 
-            if (lapide != '*') {
-                c = new Tarefa();
-                c.fromByteArray(b);
-                tarefas.add(c);
+            if( lapide != '*' ) 
+            {
+                t = new Tarefa();
+                t.fromByteArray(b);
+                tarefas.add(t);
             } // end if
         } // end while
-
-        return (tarefas);
+        return ( tarefas );
     } // end readAll ( )
 
-    // criar metodo isEmpty
-    public boolean isEmpty() throws IOException {
-        return super.isEmpty();
-    } // end isEmpty ( )
+    public List<Tarefa> readAll ( int idCategoria ) throws Exception 
+    {
+        List<Tarefa> tarefas = new ArrayList<>( );
+
+        arquivo.seek(TAM_CABECALHO);
+        byte lapide = ' ';
+        short tam = 0;
+        byte[] b = null;
+
+        Tarefa t = null;
+        while( arquivo.getFilePointer() < arquivo.length() ) 
+        {
+            lapide = arquivo.readByte();
+            tam = arquivo.readShort();
+            b = new byte[tam];
+            arquivo.read(b);
+
+            if( lapide != '*' ) 
+            {
+                t = new Tarefa();
+                t.fromByteArray(b);
+                if( t.getIdCategoria() == idCategoria ) 
+                {
+                    tarefas.add(t);
+                } // end if
+            } // end if
+        } // end while
+        return ( tarefas );
+    } // end readAll ( ) 
+    
 
     @Override
-    public boolean update(Tarefa novaTarefa) throws Exception {
+    public boolean update ( Tarefa novaTarefa ) throws Exception 
+    {
         boolean result = false;
-        Tarefa tarefaAntiga = super.read(novaTarefa.getIdCategoria());
-        if (super.update(novaTarefa)) {
-            if (novaTarefa.getIdCategoria() != tarefaAntiga.getIdCategoria()) {
-                indiceIndiretoIDCategoria
-                        .delete(new ParIDCategoriaIDTarefa(tarefaAntiga.getIdCategoria(), tarefaAntiga.getId()));
-                indiceIndiretoIDCategoria
-                        .create(new ParIDCategoriaIDTarefa(novaTarefa.getIdCategoria(), novaTarefa.getId()));
+        Tarefa tarefaAntiga = super.read( novaTarefa.getId( ) );
+        if( super.update(novaTarefa) ) 
+        {
+            if( novaTarefa.getId() != tarefaAntiga.getId() ) 
+            {
+                arvore.delete( new ParIDCategoriaIDTarefa(tarefaAntiga.getIdCategoria(), tarefaAntiga.getId()) );
+                arvore.create( new ParIDCategoriaIDTarefa(novaTarefa.getIdCategoria(), novaTarefa.getId()) );
             } // end if
             result = true;
         } // end if
         return result;
     } // end update ( )
 
-    // criar metodo update com 2 parametros
-    public boolean update(Tarefa novaTarefa, int id) throws Exception {
+    public boolean update ( Tarefa novaTarefa, int id ) throws Exception 
+    {
         boolean result = false;
-        Tarefa tarefaAntiga = super.read(id);
-        if (super.update(novaTarefa)) {
-            if (novaTarefa.getIdCategoria() != tarefaAntiga.getIdCategoria()) {
-                indiceIndiretoIDCategoria
-                        .delete(new ParIDCategoriaIDTarefa(tarefaAntiga.getIdCategoria(), tarefaAntiga.getId()));
-                indiceIndiretoIDCategoria
-                        .create(new ParIDCategoriaIDTarefa(novaTarefa.getIdCategoria(), novaTarefa.getId()));
+        Tarefa tarefaAntiga = super.read( novaTarefa.getId( ) );
+        if( super.update(novaTarefa) ) 
+        {
+            if( novaTarefa.getIdCategoria() != tarefaAntiga.getIdCategoria() ) 
+            {
+                arvore.delete( new ParIDCategoriaIDTarefa(tarefaAntiga.getIdCategoria(), tarefaAntiga.getId()) );
+                arvore.create( new ParIDCategoriaIDTarefa(novaTarefa.getIdCategoria(), novaTarefa.getId()) );
             } // end if
             result = true;
         } // end if
